1d10192f8c5896a63db271e1581c95c0e482540e
JanSternJohansen <jaj@tec.dk>
2023-03-14 08:37:25 +0100

    Nu virker Sql_Intro.sql

--- SQL_Intro.sql
+++ SQL_Intro.sql
@@ -22,14 +22,22 @@ go
 print 'Skifter til ElevDB'
 use ElevDB
 -----------------------------
+if exists(select table_name from INFORMATION_SCHEMA.TABLES where table_name = 'Elev')
+Begin
+   print 'Sletter tabellen Elev'
+   drop table Elev
+End
+go
+
 if exists (select *  from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Klasse')
 Begin
    print 'Sletter tabellen Klasse'
    drop table Klasse
 End
 go
+-------------------------------------
 
-print 'Creater tabellen Klasse'
+print 'Opretter tabellen Klasse'
 create table Klasse
 (
    K_id int primary key,
@@ -37,19 +45,6 @@ create table Klasse
    K_description varchar(50)
 )
 
-print 'Indsætter rækker i tabllen Klasse'
-insert into Klasse values(10, 'KlasseA', 'De dygtige elever')
-insert into Klasse values(20, 'KlasseB', 'De kreative elever')
-insert into Klasse values(30, 'Klassec', 'De smukke elever')
-
---------------------------------------------
-
-if exists(select table_name from INFORMATION_SCHEMA.TABLES where table_name = 'Elev')
-Begin
-   print 'Sletter tabellen Elev'
-   drop table Elev
-End
-go
 
 print 'Opretter tabellen Elev'
 create table Elev
@@ -60,6 +55,15 @@ create table Elev
    Klasse_id int foreign key references Klasse(K_id)
 )
 go
+--------------------------------------------
+
+print 'Indsætter rækker i tabllen Klasse'
+insert into Klasse values(10, 'KlasseA', 'De dygtige elever')
+insert into Klasse values(20, 'KlasseB', 'De kreative elever')
+insert into Klasse values(30, 'Klassec', 'De smukke elever')
+
+
+print 'Indsætter data i tabellen Elev'
 
 insert into Elev (id, name) values(1000, 'Samuel') --Undlade kolonne age
 insert into Elev values(1001, 'Erika', 23, 10)
@@ -68,7 +72,7 @@ insert into Elev values(1003, 'Daniel', 56, 30)
 insert into Elev values(1004,  'Anders A', 26, 10)
 insert into Elev values(1005, 'Anders H', 18, 20)
 insert into Elev values(1006, 'Anders C', 19, 20)
-insert into Elev values(1007, 'Siw', 19, 40)
+insert into Elev values(1007, 'Siw', 19, 10)
 
 
 select id, name, age, Klasse_id from Elev where name like 'A%'
